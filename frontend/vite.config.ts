4cd688e72fe1f8e2321568852f4e0a6e16f702a2
priyamehta <mehta.priyar@northeastern.edu>
2025-03-09 00:34:49 -0500

    frontend added

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-
+import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',// this willl avoid the whole path to be prefixed with '/api'
+        changeOrigin: true,
+        
+      } 
+  }
+},
+  plugins: [react(),
+    tailwindcss(),
+  ],
 })
